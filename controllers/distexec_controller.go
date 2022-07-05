df1e426022a063b927a1b2ee099602387045497f
gu18168 <gu18168@gmail.com>
2022-07-05 06:48:23 -0400

    feat: add RESTClient to exec in Pod
    more to see: https://github.com/kubernetes-sigs/kubebuilder/issues/803

--- controllers/distexec_controller.go
+++ controllers/distexec_controller.go
@@ -17,9 +17,14 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"strings"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -27,10 +32,17 @@ import (
 	execv1 "github.com/gu18168/dist-exe/api/v1"
 )
 
+const (
+	podNamespace = "dist-exec"
+	podLabel     = "dist-exec-pod"
+)
+
 // DistExecReconciler reconciles a DistExec object
 type DistExecReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	RESTClient rest.Interface
+	RESTConfig *rest.Config
+	Scheme     *runtime.Scheme
 }
 
 //+kubebuilder:rbac:groups=exec.yuhong.test,resources=distexecs,verbs=get;list;watch;create;update;patch;delete
@@ -52,6 +64,39 @@ func (r *DistExecReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	// TODO(user): your logic here
 
 	return ctrl.Result{}, nil
+}
+
+// Use RESTClient to build a request and run a command in the Pod
+func (r *DistExecReconciler) execInPod(name, command string) (string, string, error) {
+	req := r.RESTClient.Post().
+		Resource("pods").
+		Name(name).
+		Namespace(podNamespace).
+		SubResource("exec").
+		Param("container", podLabel).
+		VersionedParams(&v1.PodExecOptions{
+			Command: strings.Split(command, " "),
+			Stdin:   false,
+			Stdout:  true,
+			Stderr:  true,
+		}, runtime.NewParameterCodec(r.Scheme))
+
+	var stdout, stderr bytes.Buffer
+	exec, err := remotecommand.NewSPDYExecutor(r.RESTConfig, "POST", req.URL())
+	if err != nil {
+		return "", "", err
+	}
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  nil,
+		Stdout: &stdout,
+		Stderr: &stderr,
+	})
+	if err != nil {
+		return "", "", err
+	}
+
+	return stdout.String(), stderr.String(), nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
